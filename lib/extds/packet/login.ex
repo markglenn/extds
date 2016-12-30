e75373c3d73911d0de858696fe72296f912f851b
Mark Glenn <markglenn@gmail.com>
2016-12-30 15:00:58 -0600

    Add login to_packet method

--- lib/extds/packet/login.ex
+++ lib/extds/packet/login.ex
@@ -15,5 +15,63 @@ defmodule ExTds.Packet.Login do
       Bitwise.bxor(c, 0xA5)
     end)
     |> :binary.list_to_bin
+    |> to_string
   end
+
+  def to_packet(packet = %ExTds.Packet.Login{}) do
+    header = <<
+      0x04, 0x00, 0x00, 0x74,         # TDS Version
+      4096 :: little-size(4)-unit(8), # Packet Size
+      0x04, 0x00, 0x00, 0x07,         # Client Program Version
+      0x00, 0x10, 0x00, 0x00,         # Client PID
+      0x00 :: little-size(4)-unit(8), # Connection ID
+      0x00,                           # Option flags 1
+      0x00,                           # Option flags 2
+      0x00,                           # Type flags
+      0x00,                           # Option flags 3
+      0xE0, 0x01, 0x00, 0x00,         # Time zone
+      0x09, 0x04, 0x00, 0x00          # Collation ID
+    >>
+
+    {header, body, size} =
+      {header, <<>>, 94}
+      |> add_variable_length_string("")      # Client hostname
+      |> add_variable_length_string(packet.username)      # Username
+      |> add_variable_length_binary(encrypt_password(packet.password))      # Password
+      |> add_variable_length_string("")      # App name
+      |> add_variable_length_string("")      # Server name
+      |> add_variable_length_string("")      # Unused
+      |> add_variable_length_string("")      # Library name
+      |> add_variable_length_string("")      # Language
+      |> add_variable_length_string("")      # Database name
+      |> add_fixed_length_field(<<0x00 :: size(48)>>) # Client ID
+      |> add_variable_length_string()        # SSPI
+      |> add_variable_length_string()        # Attach DB File
+      |> add_variable_length_string()        # Change password
+      |> add_fixed_length_field(<<0x00 :: size(32)>>) # SSPI Long
+
+    <<size :: little-size(32)>> <> header <> body
+  end
+
+  defp add_variable_length_string(state, s), do: add_variable_length_binary(state, to_ucs2(s))
+  defp add_variable_length_string({header, body, offset}) do
+    {
+      header <> <<0 :: little-size(16), 0 :: little-size(16)>>,
+      body,
+      offset
+    }
+  end
+
+  defp add_variable_length_binary({header, body, offset}, s) do
+    len = byte_size(s)
+
+    {
+      header <> <<offset :: little-size(16), len >>> 1 :: little-size(16)>>,
+      body <> s,
+      offset + len
+    }
+  end 
+
+
+  defp add_fixed_length_field({header, body, offset}, field), do: {header <> field, body, offset}
 end
--- test/extds/packet/login_test.exs
+++ test/extds/packet/login_test.exs
@@ -11,5 +11,41 @@ defmodule ExTdsTest.ExTds.Packet.Login do
       assert <<0xE0, 0xA5, 0xF1, 0xA5, 0x90, 0xA5, 0xE0, 0xA5>> == Login.encrypt_password("TEST")
     end
   end
+
+  describe "to_packet" do
+    test "header with username and password" do
+      login_packet = %Login{hostname: "server.example.com", username: "exampleuser", password: "password"}
+
+      packet = Login.to_packet(login_packet)
+
+      # Size
+      <<size :: little-size(32), tail :: binary>> = packet
+      assert size == byte_size(packet)
+
+      # TDS Version
+      assert <<0x04, 0x00, 0x00, 0x74, tail :: binary>> = tail
+
+      # Message Size
+      assert <<0x00, 0x10, 0x00, 0x00, tail :: binary>> = tail
+
+      # Client Program Version
+      assert <<0x04, 0x00, 0x00, 0x07, tail :: binary>> = tail
+
+      # Client PID
+      assert <<0x00, 0x10, 0x00, 0x00, tail :: binary>> = tail
+
+      # Connection ID
+      assert <<0x00, 0x00, 0x00, 0x00, tail :: binary>> = tail
+
+      # Option flags
+      assert <<0x00, 0x00, 0x00, 0x00, tail :: binary>> = tail
+
+      # Time zone
+      assert <<0xE0, 0x01, 0x00, 0x00, tail :: binary>> = tail
+
+      # Collation ID
+      assert <<0x09, 0x04, 0x00, 0x00, _tail :: binary>> = tail
+    end
+  end
 end
 
