d76a17db79282d1c7754da4da4d2fb7f76c06c08
Mark Glenn <markglenn@gmail.com>
2017-01-05 13:10:15 -0600

    Add row parsing

--- lib/extds/response/column_metadata.ex
+++ lib/extds/response/column_metadata.ex
@@ -6,27 +6,60 @@ defmodule ExTds.Response.ColumnMetadata do
   defstruct [:count]
 
   def parse(response, column_count) do
-    <<
-      _ek_value_count :: binary-size(2)-unit(8),
-      tail :: binary
-    >> = response
+    {columns, tail} = parse_columns(response, column_count)
+    columns = Enum.reverse columns
+    row = parse_row(tail, columns)
 
-    parse_columns(tail, column_count)
+    %{columns: columns, row: Enum.reverse(row)}
   end
 
-  defp parse_columns(tail, 0), do: []
   defp parse_columns(tail, count) do
-    columns = []
-
-    parse_column(tail, count)
+    do_parse_columns([], count, tail)
   end
 
-  defp parse_column(_, 0), do: {}
-  defp parse_column(<<usertype :: little-size(2)-unit(8), flags::size(16), tail::binary>> = r, count) do
+  defp do_parse_columns(columns, 0, tail), do: {columns, tail}
+  defp do_parse_columns(columns, count, tail) do
+    {column, tail} = parse_column(tail)
+    do_parse_columns([column | columns], count - 1, tail)
+  end
+
+  defp parse_column(<<_usertype :: little-size(4)-unit(8), _flags :: size(16), tail :: binary>>) do
     {type, tail} = ExTds.Type.parse(tail)
     <<name_size :: size(8), name :: binary-size(name_size)-unit(16), tail :: binary>> = tail
 
-    << _ :: little-size(16), tail :: binary>> = tail
-    parse_column(tail, count - 1)
+    {%{name: ExTds.Utils.ucs2_to_utf(name), type: type}, tail}
+  end
+
+  defp parse_row(<<0xD2, tail :: binary>>, columns) do
+    byte_count =
+      Enum.count(columns) / 8
+      |> Float.ceil
+      |> round
+
+    <<null_bits :: binary-size(byte_count), tail :: binary>> = tail
+    null_columns = bits_to_array(null_bits)
+
+    parse_columns(tail, columns, null_columns)
+  end
+
+  defp parse_columns(<<tail :: binary>>, columns, null_columns), do: parse_columns(<<tail :: binary>>, columns, null_columns, [])
+
+  defp parse_columns(<<tail :: binary>>, [], _null_columns, record_columns), do: record_columns
+  defp parse_columns(<<tail :: binary>>, [column | columns], [1 | null_columns], record_columns) do
+    parse_columns(tail, columns, null_columns, [nil | record_columns])
+  end
+
+  defp parse_columns(<<tail :: binary>>, [column | columns], [_ | null_columns], record_columns) do
+    {value, tail} = ExTds.Type.parse_value(tail, column.type)
+
+    parse_columns(tail, columns, null_columns, [value | record_columns])
+  end
+
+  defp bits_to_array(bits), do: bits_to_array(bits, [])
+  defp bits_to_array(<<>>, bit_array), do: bit_array
+  defp bits_to_array(<<byte :: binary-size(1), tail :: binary>>, bit_array) do
+    bits = for <<bit::1 <- byte>>, do: bit
+    bit_array = bit_array ++ Enum.reverse(bits)
+    bits_to_array(tail, bit_array)
   end
 end
--- lib/extds/type.ex
+++ lib/extds/type.ex
@@ -1,5 +1,6 @@
 defmodule ExTds.Type do
   alias __MODULE__
+  @year_1900 :calendar.date_to_gregorian_days({1900,1,1})
 
   # Fixed length types
   def parse(<<0x1F, tail :: binary>>), do: {%{type: :nil,       sqltype: :null},          tail}
@@ -19,21 +20,22 @@ defmodule ExTds.Type do
 
   def parse(<<0x26, size, tail :: binary>>) do
     sqltype = case size do
-      1 -> :tinyint
-      2 -> :smallint
-      4 -> :int
-      8 -> :bigint
+      1 -> :tinyintn
+      2 -> :smallintn
+      4 -> :intn
+      8 -> :bigintn
     end
 
     type = %{
       type: :integer,
+      size: size,
       sqltype: sqltype
     }
 
     {type, tail}
   end
 
-  def parse(<<0x68, _size, tail :: binary>>), do: {%{type: :boolean, sqltype: :bitntype}, tail}
+  def parse(<<0x68, _size, tail :: binary>>), do: {%{type: :boolean, sqltype: :bitn}, tail}
   def parse(<<0x6A, size, precision, scale, tail :: binary>>) do
     {
       %{type: :decimal, precision: precision, scale: scale, sqltype: :decimal},
@@ -74,5 +76,47 @@ defmodule ExTds.Type do
 
   defp parse_collation(<<code_page :: little-size(16), flags :: little-size(16), charset>>) do
     %{ code_page: code_page, flags: flags, charset: charset }
+  end
+
+  def parse_value(tail, %{sqltype: :char}), do: parse_value(tail, %{sqltype: :nvarchar})
+  def parse_value(tail, %{sqltype: :nvarchar}) do
+    <<size :: little-size(16), value :: binary-size(size), tail :: binary>> = tail
+
+    {ExTds.Utils.ucs2_to_utf(value), tail}
+  end
+
+  # Booleans
+  def parse_value(<<bit, tail :: binary>>, %{sqltype: :bit}), do: {bit == 0x01, tail}
+  def parse_value(<<0x01, bit, tail :: binary>>, %{sqltype: :bitn}), do: {bit != 0x00, tail}
+  def parse_value(<<0x00, tail :: binary>>, %{sqltype: :bitn}), do: {nil, tail}
+  
+  # Integers
+  def parse_value(<<value :: little-signed-size(32), tail :: binary>>, %{sqltype: :int}), do: {value, tail}
+  def parse_value(<<value, tail :: binary>>, %{sqltype: :tinyint}), do: {value, tail}
+  def parse_value(<<size, value :: little-signed-size(size)-unit(8), tail :: binary>>, %{sqltype: :tinyintn, size: size}), do: {value, tail}
+  def parse_value(<<size, value :: little-signed-size(size)-unit(8), tail :: binary>>, %{sqltype: :smallintn, size: size}), do: {value, tail}
+  def parse_value(<<size, value :: little-signed-size(size)-unit(8), tail :: binary>>, %{sqltype: :intn, size: size}), do: {value, tail}
+  def parse_value(<<size, value :: little-signed-size(size)-unit(8), tail :: binary>>, %{sqltype: :bigintn, size: size}), do: {value, tail}
+
+  def parse_value(<<days :: little-signed-32, seconds_300 :: little-unsigned-32, tail :: binary>>, %{sqltype: :datetime}) do
+    year_1900 = 
+      {1900,1,1}
+      |> :calendar.date_to_gregorian_days
+
+    date =
+      year_1900 + days
+      |> :calendar.gregorian_days_to_date
+
+    seconds = div(seconds_300, 300)
+
+    {_, {h, m, s}} = 
+      seconds
+      |> :calendar.seconds_to_daystime
+
+    sub_seconds = (seconds_300 / 300) - seconds
+
+    micro_seconds = (sub_seconds * 1000000 + 0.5) |> trunc
+
+    {{date, {h, m, s, micro_seconds}}, tail}
   end
 end
